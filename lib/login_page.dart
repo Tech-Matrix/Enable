9eb81ebb471b663d746f54fba44f185b187eb579
FlutLab.io <>
2021-02-27 06:28:42 +0000

    Autosave

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,11 +2,52 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'auth.dart';
 
+import 'dart:async';
+
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
+
+
+class AuthService with ChangeNotifier {
+  var currentUser;
+
+  AuthService() {
+    print("new AuthService");
+  }
+
+  Future getUser() {
+    return Future.value(currentUser);
+  }
+
+  // wrappinhg the firebase calls
+  Future logout() {
+    this.currentUser = null;
+    notifyListeners();
+    return Future.value(currentUser);
+  }
+
+  // wrapping the firebase calls
+  Future createUser(
+      {String firstName,
+      String lastName,
+      String email,
+      String password}) async {}
+
+  // logs in the user if password matches
+  Future loginUser({String email, String password}) {
+    if (password == 'password123') {
+      this.currentUser = {'email': email};
+      notifyListeners();
+      return Future.value(currentUser);
+    } else {
+      this.currentUser = null;
+      return Future.value(null);
+    }
+  }
+}
 class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
   String _password;
